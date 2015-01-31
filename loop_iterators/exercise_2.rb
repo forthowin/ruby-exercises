69c72825344d1249a2ecb6fde621617929395b2a
Tho Nguyen <thogotmail@gmail.com>
2015-01-30 18:06:51 -0600

    Finish exercises for loop iterators

---
+++ loop_iterators/exercise_2.rb
@@ -0,0 +1,5 @@
+while 1
+  puts "Type 'STOP' to stop."
+  input = gets.chomp
+  break if input == "STOP"
+end
---
+++ loop_iterators/exercise_3.rb
@@ -0,0 +1,5 @@
+a = ["bill", "bob", "joe"]
+
+a.each_with_index do |name, index|
+  puts "#{index + 1}. #{name}"
+end
